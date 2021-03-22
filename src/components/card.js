d0d2b62c75c626e6ab11728aeb626716e3105f81
Alex Krantz <alex@krantz.dev>
2021-03-22 13:35:10 -0700

    Remove links from track cards

--- src/components/card.js
+++ src/components/card.js
@@ -3,16 +3,11 @@ import PropTypes from "prop-types"
 
 const Card = ({ header, content }) => (
   <div className="w-1/2 md:w-1/4 p-6 flex flex-col flex-grow flex-shrink">
-    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-      <a
-        href="https://www.gatsbyjs.org/"
-        className="flex flex-wrap no-underline hover:no-underline"
-      >
-        <div className="w-full font-bold text-xl text-gray-800 px-6 pt-6">
-          {header}
-        </div>
-        <p className="text-gray-800 text-base px-6 pt-6 mb-5">{content}</p>
-      </a>
+    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+      <div className="w-full font-bold text-xl text-gray-800 px-6 pt-6">
+        {header}
+      </div>
+      <p className="text-gray-800 text-base px-6 pt-6 mb-5">{content}</p>
     </div>
   </div>
 )
