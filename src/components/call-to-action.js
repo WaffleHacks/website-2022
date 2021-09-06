896b050490f776f15645f85db01ed010b7fccda4
Alex Krantz <alex@krantz.dev>
2021-09-06 12:08:38 -0700

    Fix countdown

--- src/components/call-to-action.js
+++ src/components/call-to-action.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import { DateTime } from "luxon"
 
 import Countdown from "./countdown"
 
@@ -19,15 +20,12 @@ const CallToAction = () => {
       homepage: { countdown_to },
     },
   } = useStaticQuery(query)
-  return (
-    <section className="container mx-auto text-center pt-8 pb-12">
-      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
-        WaffleHacks
-      </h1>
-      <div className="w-full mb-4">
-        <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t" />
-      </div>
 
+  const parsed = DateTime.fromISO(countdown_to).toLocal();
+  const completed = parsed < DateTime.local();
+
+  const countdown = (
+    <>
       <Countdown time={countdown_to} />
 
       <br />
@@ -37,6 +35,19 @@ const CallToAction = () => {
       >
         Register
       </a>
+    </>
+  )
+
+  return (
+    <section className="container mx-auto text-center pt-8 pb-12">
+      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
+        WaffleHacks
+      </h1>
+      <div className="w-full mb-4">
+        <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t" />
+      </div>
+
+      {completed ? <p className="text-xl h-3 pb-12">Registration has closed.<br/>We look forward to seeing you next year!</p> : countdown}
     </section>
   )
 }
