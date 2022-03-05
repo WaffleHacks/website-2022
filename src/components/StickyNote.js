117a6e715ee3f0a8a7678fafd881924545fea021
Ethan Horowitz <ethan.horowitz@duke.edu>
2022-03-05 11:30:31 -0500

    tracks mobile optimization

--- src/components/StickyNote.js
+++ src/components/StickyNote.js
@@ -5,7 +5,7 @@ import note_tape from "../images/note_tape.svg"
 
 const StickyNote = ({ img, name }) => {
   return (
-    <div style={{margin: '1rem 1rem 3rem 1rem', position: 'relative', padding: '10%', boxSizing: 'border-box'}}>
+    <div class='trackbox' style={{margin: '1rem 1rem 3rem 1rem', position: 'relative', padding: '10%', boxSizing: 'border-box'}}>
       <img
         src={note_tape}
         alt=""
@@ -18,8 +18,8 @@ const StickyNote = ({ img, name }) => {
       />
       <img src={note_paper} alt="sticky note" style={{width: '100%'}} />
       <div style={{position: 'absolute', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', width: '100%', left: '0%', top: '0%'}} >
-        <img src={img} alt="waffle stack" style={{maxWidth: '100%', maxHeight: '60%'}} />
-        <span style={{fontSize: '1.5em'}}>{name}</span>
+        <img src={img} alt="waffle stack" style={{height: '100%', maxWidth: '70%', maxHeight: '60%'}} />
+        <span style={{fontSize: '2em'}}>{name}</span>
       </div>
     </div>
   )
