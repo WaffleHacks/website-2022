7b1d0de76fb09e34787fe19f4b9cd39f614c79cc
Rachelle Hu <rachelle.hu123@gmail.com>
2022-03-11 23:42:03 -0500

    more precise width

--- src/components/FaqPointer.js
+++ src/components/FaqPointer.js
@@ -1,23 +1,25 @@
 import * as React from "react";
-import faq_pointer from '../images/faq_pointer.svg';
+import faq_pointer from "../images/faq_pointer.svg";
 
-let pointerWid = '30vw';
+let pointerWid = "28vw";
 
 let pointerStyles = {
-    width: pointerWid, 
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-    margin: '1.6vw 1vw'
-}
+  width: pointerWid,
+  display: "flex",
+  alignItems: "center",
+  position: "relative",
+  margin: "1.6vw 1vw",
+};
 
 const FaqPointer = ({ question, desc }) => {
   return (
-    <div className='faq-pointer' style={pointerStyles}>
-        <img src={faq_pointer} alt="" style={{width: pointerWid}} />
-        <span style={{position: 'absolute', left: '1rem', fontSize: '1.5vw'}}>{question}</span>
+    <div className="faq-pointer" style={pointerStyles}>
+      <img src={faq_pointer} alt="" style={{ width: pointerWid }} />
+      <span style={{ position: "absolute", left: "1rem", fontSize: "1.5vw" }}>
+        {question}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default FaqPointer
+export default FaqPointer;
